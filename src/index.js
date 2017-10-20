b9767e2289842598d08b366c6b87c0e046a780f4
Alfredo Miranda <alfredo@miranda.io>
2017-10-20 13:47:11 -0500

    Refactor entry point

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import todoApp from './reducers';
-import App from './components/App';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-const persistedState = loadState();
-
-const store = createStore(todoApp, persistedState);
-
-store.subscribe(throttle(() => {
-  saveState({
-    todos: store.getState().todos
-  });
-}, 1000));
+const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
