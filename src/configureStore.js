4fac1c9f6578d2c33bcf5579b9d73eb617d4f9e5
Alfredo Miranda <alfredo@miranda.io>
2017-12-02 10:52:49 -0500

    Apply Redux middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,29 +3,6 @@ import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-const logger = (store) => (next) => {
-  if (!console.group) {
-    return next;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  }
-}
-
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    return action.then(next);
-  }
-  return next(action);
-};
-
 const configureStore = () => {
   const middlewares = [promise];
 
