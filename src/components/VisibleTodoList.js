c600323bca48ca624a750e25118126015dad99e1
Alfredo Miranda <alfredo@miranda.io>
2017-12-29 14:20:05 -0500

    Display error messages

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import * as actions from '../actions';
-import { getVisibleTodos, getIsFetching } from '../reducers';
+import { getVisibleTodos, getErrorMessage, getIsFetching } from '../reducers';
 import TodoList from './TodoList';
+import FetchError from './FetchError';
 
 class VisibleTodoList extends Component {
   componentDidMount() {
@@ -22,9 +23,18 @@ class VisibleTodoList extends Component {
   }
 
   render() {
-    const { toggleTodo, todos, isFetching } = this.props;
+    const { toggleTodo, errorMessage, todos, isFetching } = this.props;
     if (isFetching && !todos.length) {
       return <p>Loading...</p>;
+    }
+
+    if (errorMessage && !todos.length) {
+      return (
+        <FetchError
+          message={errorMessage}
+          onRetry={() => this.fetchData()}
+        />
+      );
     }
 
     return (
@@ -42,6 +52,7 @@ const mapStateToProps = (state, { params }) => {
   return {
     todos: getVisibleTodos(state, filter),
     isFetching: getIsFetching(state, filter),
+    errorMessage: getErrorMessage(state, filter),
     filter
   };
 };
